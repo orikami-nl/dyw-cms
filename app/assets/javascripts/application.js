2cb6943a808e33901070e02fe73971d04a75864f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-05-20 13:41:15 +0200

    Overerving css gefixed. Thijs

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-// FIXME: Tell people that this is a manifest file, real code should go into discrete files
-// FIXME: Tell people how Sprockets and CoffeeScript works
-//
-<% unless options[:skip_javascript] -%>
-//= require <%= options[:javascript] %>
-//= require <%= options[:javascript] %>_ujs
-<% end -%>
-//= require_tree .
