f35351e30e9036c4729060e7bf46048e8b278d7e
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-05-30 14:00:24 +0200

    Screwing with routings, Thijs

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -2,5 +2,19 @@ module Cms
   class Page < ActiveRecord::Base
 		validates_presence_of :title, :link_url
 		validates_uniqueness_of :title, :link_url
+
+		after_save :update_routes
+		after_destroy :update_routes
+
+		def update_routes
+			Rails.application.routes.draw do |map|
+				Page.all.each do |page|
+   				match 'cms/pages/:id', :to => 'page#show', :as => page.link_url
+				end
+				#mount Cms::Engine => "/cms"
+			end
+			Rails.application.routes.reload!	    
+			#ActionController::Routing::Routes.reload!
+		end
   end
 end
