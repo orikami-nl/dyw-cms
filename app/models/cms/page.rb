0619a4a04c1e91e945e43540aa06c2c257602639
Andres Lamont <alamont@gmail.com>
2011-05-31 11:33:59 +0200

    Add dynamic routes to pages. Does not work yet.

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -2,5 +2,30 @@ module Cms
   class Page < ActiveRecord::Base
 		validates_presence_of :title, :link_url
 		validates_uniqueness_of :title, :link_url
+    
+    after_save :update_route
+
+    def add_routeC
+      new_route = ActionDispatch::Routing::RouteSet.add_route(title, route_options)
+      ActionDispatch::Routing::Routes.routes.insert(0, new_route)
+    end
+  
+  def remove_route
+      ActionDispatch::Routing::RouteSet.clear! { |r| r.instance_variable_get(:@requirements)[:page_id] == id }
+    end
+  
+    def update_route
+      remove_route
+      add_route
+    end
+    
+    def route_options
+      @route_options ||= { :controller     => link.controller, 
+                           :action         => link.action, 
+                           :link_id        => link_id,
+                           :page_id        => id }
+  end
+
+
   end
 end
