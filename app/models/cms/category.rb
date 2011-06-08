175e8b5f5287d3f0f48df484e598e70a1c2d9383
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-06-08 17:05:32 +0200

    Validatie voor geen spaties in link_name

--- app/models/cms/category.rb
+++ app/models/cms/category.rb
@@ -3,5 +3,6 @@ module Cms
 		has_many :pages, :dependent => :destroy
 		validates_presence_of :category_title, :link_name
 		validates_uniqueness_of :category_title, :link_name
+		validates_format_of :link_name, :with => /^[A-Za-z\d_]+$/
   end
 end
--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -3,5 +3,6 @@ module Cms
 		belongs_to :category
 		validates_presence_of :page_title, :link_name
 		validates_uniqueness_of :page_title, :link_name
+		validates_format_of :link_name, :with => /^[A-Za-z\d_]+$/
   end
 end
