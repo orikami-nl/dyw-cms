70b09080839d8de1ee39b31e0d278bddfb4c825c
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-07-02 17:37:27 +0200

    Uniqueness validation op pages uitgezet

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -3,7 +3,6 @@ module Cms
 		belongs_to :category
 		attr_accessible :page_title, :link_name, :position, :body
 		validates_presence_of :page_title, :link_name
-		validates_uniqueness_of :page_title, :link_name
 		validates_format_of :link_name, :with => /^[A-Za-z\d_]+$/
 
 		def previous_page(category)
